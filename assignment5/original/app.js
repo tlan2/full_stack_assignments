d4dc9e41ac11403558bfb80829e3fec6ef78aaf6
Tom Lancaster <tlan2@pdx.edu>
2019-11-15 09:31:28 -0800

    Completed Assignment 5

---
+++ assignment5/original/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const port = 8080
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/"+'index.html');
+});
+
+app.post('/submit', function(req, res) {
+    var iname = req.body.inputName;
+    var iemail = req.body.inputEmail;
+    var icomment = req.body.inputComment;
+
+    res.write("name: " + iname + "\n");
+    res.write("email: " + iemail + "\n");
+    res.write("name: " + icomment + "\n");
+    res.end()
+});
+
+var server = app.listen(port, function () {
+    console.log('Node server is running..');
+});
